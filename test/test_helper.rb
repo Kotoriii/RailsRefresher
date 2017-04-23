2b42f2ddbd27174ed36e483221f1d9da3d022e58
Kotoriii <cmackm@hotmail.com>
2017-04-23 04:14:40 -0600

    authorization, only allow specific user to edit their own profile info

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,10 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   def is_logged_in?
- 		!session[:user_id].nil?
- 	end
+  	!session[:user_id].nil?
+  end
+
+ #  def log_in_as(user)
+ #    session[:user_id] = user.id    
+	# end
 end
